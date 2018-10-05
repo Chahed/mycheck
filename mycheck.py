574b5957acaff1c4f822ce2149d36ec22d6aafbc
Hedi Chahed <hedi.chahed.2i@gmail.com>
2018-10-05 19:36:40 +0100

    add docstrings and improve code quality

--- mycheck.py
+++ mycheck.py
@@ -1,10 +1,15 @@
-
-import sys    
-import json  
-import socket
+"""
+Program that checks connectivity to sites
+"""
+import sys     # dependency used to get arguments
+import json    # dependency used to convert text to json
+import socket  # dependency used to create connection to tcp services
 
 
 def check_connection(hostname):
+    """
+    Connect to a TCP service listening on the internet address
+    """
     ip_address = hostname.split(':')[0]
     port = hostname.split(':')[1]
     try:
@@ -16,14 +21,17 @@ def check_connection(hostname):
 
 
 def check_connectivity_file(file_name):
+    """
+    Open file and get hostname to test connectivity
+    """
     file_obj = open(file_name, "r")
     content = json.loads(file_obj.read())
     sites = content['checks']['ping']
     for name, hostname in sites.items():
         if check_connection(hostname):
-            output = 'ok {}'
+            output = 'ok : {}'
         else:
-            output = 'No {}'
+            output = 'No : {}'
         print(output.format(name))
 
 if __name__ == '__main__':
