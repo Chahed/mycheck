52292d66eb52d82a32bba3213c843e89169736fa
Hedi Chahed <hedi.chahed.2i@gmail.com>
2018-10-05 19:32:24 +0100

    add new function that check connection to hostname
    it checks connection using python dependency "socket"

--- mycheck.py
+++ mycheck.py
@@ -1,7 +1,30 @@
-import sys
+
+import sys    
+import json  
+import socket
+
+
+def check_connection(hostname):
+    ip_address = hostname.split(':')[0]
+    port = hostname.split(':')[1]
+    try:
+        socket.create_connection((ip_address, port))
+        return True
+    except OSError:
+        pass
+    return False
+
 
 def check_connectivity_file(file_name):
-  print(file_name)
+    file_obj = open(file_name, "r")
+    content = json.loads(file_obj.read())
+    sites = content['checks']['ping']
+    for name, hostname in sites.items():
+        if check_connection(hostname):
+            output = 'ok {}'
+        else:
+            output = 'No {}'
+        print(output.format(name))
 
 if __name__ == '__main__':
     FILE = sys.argv[1]
